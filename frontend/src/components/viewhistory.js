d34e23c07283071082f86e2ffe272a896943a25d
Tinnapop Pratheep <tinnapop.pratheep@gmail.com>
2021-02-17 03:25:58 +0700

    Change picture to icon

--- frontend/src/components/viewhistory.js
+++ frontend/src/components/viewhistory.js
@@ -1,3 +1,4 @@
+import { faHistory } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Button } from 'antd'
 import React from 'react'
@@ -8,12 +9,7 @@ const viewhistory = (props) => {
         <div className="view-shop-btn">
             {console.log(props.test)}
             <Button className="button" style={{marginTop:"10px", width:"auto"}}>     
-                <img 
-                    width="40px"
-                    height="40px"
-                    src={icon}
-                    style={{marginRight:"15px", }}
-                />
+                <FontAwesomeIcon icon={faHistory} style={{marginRight:"10px", fontSize:"35px"}} />
                 {"Shopping history >"}
             </Button>
         </div>
