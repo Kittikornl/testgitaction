536dc7744eecd3f5dbe1c95a00fa9527e4648fcc
Tinnapop Pratheep <tinnapop.pratheep@gmail.com>
2021-02-17 03:12:20 +0700

    change picture to icon

--- frontend/src/components/viewshopbutton.js
+++ frontend/src/components/viewshopbutton.js
@@ -1,3 +1,5 @@
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Button } from 'antd'
 import React from 'react'
 import icon from '../img/cart.png'
@@ -7,23 +9,13 @@ const viewshopbutton = (props) => {
         <div className="view-shop-btn">
             {(props.enable) && 
                 <Button className="button" style={{width:"auto"}}>     
-                    <img 
-                        width="40px"
-                        height="40px"
-                        src={icon}
-                        style={{marginRight:"15px", }}
-                    />
+                    <FontAwesomeIcon icon={faShoppingCart} style={{marginRight:"10px", fontSize:"35px"}} />
                     {"View shop >"}
                 </Button>
             }
             {(!props.enable) && 
                 <Button className="button" style={{width: "auto", backgroundColor: "#979797" }} disabled>
-                    <img
-                        width="40px"
-                        height="40px"
-                        src={icon}
-                        style={{ marginRight: "15px", }}
-                    />
+                    <FontAwesomeIcon icon={faShoppingCart} style={{marginRight:"10px", fontSize:"35px"}} />
                     {"View shop >"}
                 </Button>
             }
