dec0d502e31c725980acd568f03213082398f1ac
Tinnapop Pratheep <tinnapop.pratheep@gmail.com>
2021-02-17 03:22:21 +0700

    Remove dev

--- frontend/src/components/scores.js
+++ frontend/src/components/scores.js
@@ -1,57 +1,36 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faStar, faStarHalfAlt} from '@fortawesome/free-solid-svg-icons'
-import {faStar as faStarZero} from '@fortawesome/free-regular-svg-icons'
-
-const Showcase = () => (
-    <div style={{display:"flex", flexDirection:"row"}}>
-        <div className="star-group" style={{ color: "#FFC107", fontSize: "30px", paddingRight:"5px"}}>
-            <FontAwesomeIcon icon={faStar} />
-        </div>
-        <div className="star-group" style={{ color: "#FFC107", fontSize: "30px", paddingRight:"5px"}}>
-            <FontAwesomeIcon icon={faStar} />
-            <i class="fas fa-star-half"></i>
-        </div>
-        
-    </div>
-)
+import { faStar, faStarHalfAlt } from '@fortawesome/free-solid-svg-icons'
+import { faStar as faStarZero } from '@fortawesome/free-regular-svg-icons'
 
 const renderStar = (score) => {
     const intScore = parseInt(score)
-    const zeroStar = 4-intScore
+    const zeroStar = 4 - intScore
     const listStar = []
-    for(let i=0;i<intScore;i++) {
+    for (let i = 0; i < intScore; i++) {
         listStar.push(
-            <div className="star-item" style={{ color: "#FFC107", fontSize: "30px", paddingRight:"5px"}}>
-                <FontAwesomeIcon icon={faStar} />
-            </div>
+            <FontAwesomeIcon icon={faStar} style={{ color: "#FFC107", fontSize: "30px" }} />
         )
     }
-    if (score-intScore===0.5)
+    if (score - intScore === 0.5)
         listStar.push(
-            <div className="star-item" style={{ color: "#FFC107", fontSize: "30px", paddingRight:"5px"}}>
-                <FontAwesomeIcon icon={faStarHalfAlt} />
-            </div>
+            <FontAwesomeIcon icon={faStarHalfAlt} style={{ color: "#FFC107", fontSize: "30px" }} />
         )
     for (let i = 0; i < zeroStar; i++) {
         listStar.push(
-            <div className="star-item" style={{ color: "#FFC107", fontSize: "30px", paddingRight: "5px" }}>
-                <FontAwesomeIcon icon={faStarZero} />
-            </div>
+            <FontAwesomeIcon icon={faStarZero} style={{ color: "#FFC107", fontSize: "30px" }} />
         )
     }
-    if (score===0)
+    if (score === 0)
         listStar.push(
-            <div className="star-item" style={{ color: "#FFC107", fontSize: "30px", paddingRight: "5px" }}>
-                <FontAwesomeIcon icon={faStarZero} />
-            </div>
+            <FontAwesomeIcon icon={faStarZero} style={{ color: "#FFC107", fontSize: "30px" }} />
         )
     return listStar
 }
 
 const scores = (props) => {
     return (
-        <div className="star-group" style={{display:"flex", flexDirection:"row"}}>      
+        <div className="star-group flex-row" >
             {renderStar(props.score)}
         </div>
     );
