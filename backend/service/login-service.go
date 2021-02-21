63d53bfabc28986d9769045264ec8551927f400f
M.Sitthibudhi <sitthiput.m@gmail.com>
2021-02-21 14:48:52 +0700

    Edit name of var

--- backend/service/login-service.go
+++ backend/service/login-service.go
@@ -5,18 +5,19 @@ type LoginService interface {
 }
 
 type loginInformation struct {
-	email    string
-	password string
+	Email    string
+	Password string
 }
 
-// Test a token
+// Store data from database for comparing
 func NewLoginService(email string, password string) LoginService {
 	return &loginInformation{
-		email:    email,
-		password: password,
+		Email:    email,
+		Password: password,
 	}
 }
 
 func (info *loginInformation) LoginUser(email string, password string) bool {
-	return info.email == email && info.password == password
+	// Check if the input email and password match the database
+	return info.Email == email && info.Password == password
 }
