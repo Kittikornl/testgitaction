74d3b03c6e95f80259cfb17a94c3d828a315d369
M.Sitthibudhi <sitthiput.m@gmail.com>
2021-02-21 20:57:43 +0700

    Edit credential form

--- backend/dto/LoginCredentials.go
+++ backend/dto/LoginCredentials.go
@@ -2,8 +2,6 @@ package dto
 
 //Login credential
 type LoginCredentials struct {
-	Email    string `form:"email"`
-	Password string `form:"password"`
-	UserID   int    `form:"userID"`
-	Role     int    `form:"role"`
+	UserID int `form:"userID"`
+	Role   int `form:"role"`
 }
