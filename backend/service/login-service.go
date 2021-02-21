374d062be9fdd7d67b325940b61476bcd9da9d4a
M.Sitthibudhi <sitthiput.m@gmail.com>
2021-02-21 20:58:31 +0700

    Edit structure's name to prevent the error

--- backend/service/login-service.go
+++ backend/service/login-service.go
@@ -4,20 +4,19 @@ type LoginService interface {
 	LoginUser(email string, password string) bool
 }
 
-type loginInformation struct {
+type LoginInformation struct {
 	Email    string
 	Password string
 }
 
 // Store data from database for comparing
 func NewLoginService(email string, password string) LoginService {
-	return &loginInformation{
+	return &LoginInformation{
 		Email:    email,
 		Password: password,
 	}
 }
 
-func (info *loginInformation) LoginUser(email string, password string) bool {
-	// Check if the input email and password match the database
+func (info *LoginInformation) LoginUser(email string, password string) bool {
 	return info.Email == email && info.Password == password
 }
