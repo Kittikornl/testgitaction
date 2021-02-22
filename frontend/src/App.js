8a3a55a22db0cf6e2143bddca87b2f38602c8ba9
Siras Jaroenpoj <sirasjaroenpoj@MacBook-Pro-khxng-Siras.local>
2021-02-22 23:55:31 +0700

    Add editprofile path

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
-import './App.scss';
-import { Route, Router, Switch } from 'react-router-dom';
-import Login from "./login/login"
-import Register from './register/register'
-import ResetPassword from './login/resetpassword'
+import "./App.scss";
+import { Route, Router, Switch } from "react-router-dom";
+import Login from "./login/login";
+import Register from "./register/register";
+import ResetPassword from "./login/resetpassword";
 import { useHistory } from "react-router-dom";
-import Profile from './profile/profile';
+import Profile from "./profile/profile";
+import Editprofile from "./profile/Editprofile";
 
 function App() {
-  const history = useHistory()
+  const history = useHistory();
   // const location = useLocation();
   // const auth = useContext(Authentication);
   // const { currentUser } = auth;
@@ -24,11 +25,22 @@ function App() {
       <div className="flex">
         {/* {checkPath(location.pathname) ? } */}
         <Switch>
+          <Route
+            exact
+            key="editprofile"
+            path="/profile/edit"
+            component={Editprofile}
+          />
           <Route exact key="login" path="/login" component={Login} />
           <Route exact key="login" path="/" component={Login} />
           <Route exact key="register" path="/register" component={Register} />
-          <Route exact key="reset password" path="/password/reset" component={ResetPassword} />
-          <Route exact key="profile" path="/profile" component={Profile} /> 
+          <Route
+            exact
+            key="reset password"
+            path="/password/reset"
+            component={ResetPassword}
+          />
+          <Route exact key="profile" path="/profile" component={Profile} />
           {/* <Route exact key="edit profile" path="profile/edit" component={editProfile} /> */}
         </Switch>
       </div>
