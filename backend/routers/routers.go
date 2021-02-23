e190142e93a6d8ee6e5611fb4b7035ecdb83226c
pkorn <pkorn003@gmail.com>
2021-02-23 13:10:19 +0700

    Set cors

--- backend/routers/routers.go
+++ backend/routers/routers.go
@@ -1,12 +1,17 @@
 package routers
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/sec33_Emparty/backend/handle/user"
 )
 
 func InitRouter() *gin.Engine {
 	r := gin.Default()
+	corsConfig := cors.DefaultConfig()
+	corsConfig.AllowOrigins = []string{"*"}
+	corsConfig.AllowCredentials = true
+	corsConfig.AddAllowMethods("*")
 	r.GET("/api/users", user.GetAllUser)
 	r.GET("/api/accounts", user.GetAllAccount)
 	r.POST("/api/users", user.SaveUser)
@@ -17,6 +22,6 @@ func InitRouter() *gin.Engine {
 	r.GET("/api/users/:id", user.GetUser)
 	r.PUT("/api/users/:id", user.UpdateUser)
 	r.PATCH("/api/users/:id/change-pwd", user.ChangePassword)
-
+	r.Use(cors.New(corsConfig))
 	return r
 }
