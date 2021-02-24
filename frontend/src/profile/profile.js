e98c800c742b40fd33cd868645f760761401b665
Tinnapop Pratheep <tinnapop.pratheep@gmail.com>
2021-02-24 11:28:49 +0700

    Edit profile icon href add

--- frontend/src/profile/profile.js
+++ frontend/src/profile/profile.js
@@ -98,7 +98,7 @@ const Profile = () => {
                 <div className="profile-title flex-col">
                     <div className="full-name">
                         <div className="name">{`${data.firstname} ${data.lastname}`}</div>
-                        <a className="edit-info" href="/editprofile"><FontAwesomeIcon icon={faUserEdit} />&nbsp;<div>Edit Profile</div></a>
+                        <a className="edit-info" href="/profile/edit"><FontAwesomeIcon icon={faUserEdit} />&nbsp;<div>Edit Profile</div></a>
                     </div>
                     <div className="role">{renderRole(role)}</div>
                     <div className="info-container flex-col">
