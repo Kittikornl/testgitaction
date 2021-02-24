9a581fad99eb369bd409088b06a1db615b9a4cb4
mintcnn <mintchnrt@gmail.com>
2021-02-24 12:17:38 +0700

    connect reset pwd and add component noti

--- frontend/src/login/resetpassword.js
+++ frontend/src/login/resetpassword.js
@@ -3,6 +3,7 @@ import { Button, Input, Form } from 'antd'
 import { Link, useHistory } from 'react-router-dom'
 import "./resetpassword.scss"
 import { postResetPassword } from '../service/user.service'
+import Notification from '../components/notification'
 
 const ResetPassword = () => {
     const history = useHistory()
@@ -11,7 +12,7 @@ const ResetPassword = () => {
         try {
             console.log(e)
             const res = await postResetPassword(e)
-            console.log(res);
+            Notification({type :'success', message:'reset password', desc:'Your new password was sent to your email'} )
             history.push('/login')
         } catch (error) {
             console.log('error')
