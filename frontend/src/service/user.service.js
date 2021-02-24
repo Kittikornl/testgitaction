e6adf98ccf79ade631fb1b1e08ddf81873a5efef
Siras Jaroenpoj <sirasjaroenpoj@MacBook-Pro-khxng-Siras.local>
2021-02-24 10:34:12 +0700

    add getUserdata and change patch to put

--- frontend/src/service/user.service.js
+++ frontend/src/service/user.service.js
@@ -1,58 +1,66 @@
-import axios from 'axios'
-import authHeader from './auth-header'
+import axios from "axios";
+import authHeader from "./auth-header";
 
-const API_URL = 'http://localhost:8080/api'
+const API_URL = "http://localhost:8080/api";
+
+export const getUserData = async (id) => {
+  try {
+    return await axios.get(API_URL + `/users/${id}`);
+  } catch (error) {
+    console.log("error get userdata");
+    throw error;
+  }
+};
 
 export const postRegister = async (values) => {
-    try {
-        console.log(values);
-        return await axios.post(API_URL + '/users', values)
-    } catch (error) {
-        console.log('error register')
-        throw error
-    }
-}
+  try {
+    return await axios.post(API_URL + "/users", values);
+  } catch (error) {
+    console.log("error register");
+    throw error;
+  }
+};
 
 export const postChangePassword = async (userId, values) => {
-    try {
-        return await axios.post(`/users/${userId}`, values, {
-            headers: authHeader() 
-        })
-    } catch (error) {
-        console.log('error change password')
-        throw error
-    }
-}
+  try {
+    return await axios.post(API_URL + `/users/${userId}`, values, {
+      headers: authHeader(),
+    });
+  } catch (error) {
+    console.log("error change password");
+    throw error;
+  }
+};
 
-export const patchEditProfile = async (userId, values) => {
-    try {
-        return await axios.patch(API_URL +`/users/${userId}`, values, {
-            headers: authHeader() 
-        })
-    } catch (error) {
-        console.log('error edit profile')
-        throw error
-    }
-}
+export const putEditProfile = async (userId, values) => {
+  try {
+    return await axios.put(API_URL + `/users/${userId}`, values, {
+      headers: authHeader(),
+    });
+  } catch (error) {
+    console.log("error edit profile");
+    throw error;
+  }
+};
 
 export const deleteProfile = async (userId) => {
-    try {
-        return await axios.delete(`/users/${userId}`, {
-            headers: authHeader() 
-        })
-    } catch (error) {
-        console.log('error delete profile')
-        throw error
-    }
-}
- 
+  try {
+    return await axios.delete(API_URL + `/users/${userId}`, {
+      headers: authHeader(),
+    });
+  } catch (error) {
+    console.log("error delete profile");
+    throw error;
+  }
+};
+
 export const postResetPassword = async (values) => {
-    try {
-        return await axios.post(API_URL + '/users/reset-pwd', values, {
-            headers: authHeader() 
-        })
-    } catch (error) {
-        console.log('error reset password')
-        throw error
-    }
-}
+  try {
+    return await axios.post(API_URL + "/users/reset-pwd", values, {
+      headers: authHeader(),
+    });
+  } catch (error) {
+    console.log("error reset password");
+    throw error;
+  }
+};
