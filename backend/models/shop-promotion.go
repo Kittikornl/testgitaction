2ea8f11d86456bbcd38050fc958fa831be6a6dff
M.Sitthibudhi <sitthiput.m@gmail.com>
2021-03-05 12:18:36 +0700

    Edit bug: using () instead of {}

--- backend/models/shop-promotion.go
+++ backend/models/shop-promotion.go
@@ -1,12 +1,12 @@
 package models
 
-import {
+import (
 	"gorm.io/gorm"
-}
+)
 
 type ShopPromotion struct {
 	// Auto-generated Promotion ID
-	Shop	shop		`gorm:"foreignKey: id; constraint:OnUpdate:RESTRICT,OnDelete:RESTRICT;"`
+	Shop shop `gorm:"foreignKey: id; constraint:OnUpdate:RESTRICT,OnDelete:RESTRICT;"`
 	// Wait for promotion to be created, may edit later
 	Promotion promotion `gorm:"foreignKey: id; constraint:OnUpdate:RESTRICT,OnDelete:RESTRICT;"`
 }
