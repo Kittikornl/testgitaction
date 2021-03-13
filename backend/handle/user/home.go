dad0df59b210138b90ea64ba0fa222fa45c1e499
pkorn <pkorn003@gmail.com>
2021-03-14 03:10:21 +0700

    Remove not use function

--- backend/handle/user/home.go
+++ backend/handle/user/home.go
@@ -4,27 +4,9 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/gin-gonic/gin/binding"
 	"github.com/sec33_Emparty/backend/database"
 	"github.com/sec33_Emparty/backend/models"
 )
-
-func AddSoldProduct(c *gin.Context) {
-	product := models.Soldproduct{}
-
-	if err := c.ShouldBindBodyWith(&product, binding.JSON); err != nil {
-		c.Status(http.StatusBadRequest)
-		println(err.Error())
-		return
-	}
-
-	if err := database.DB.Save(&product).Error; err != nil {
-		c.Status(http.StatusInternalServerError)
-		println(err.Error())
-		return
-	}
-	c.JSON(http.StatusOK, &product)
-}
 
 func GetHomePage(c *gin.Context) {
 	new_products := []models.Product{}
