87ff114bbcedab344372a44c455cb178cc637316
pkorn <pkorn003@gmail.com>
2021-03-14 01:12:03 +0700

    Rename table

--- backend/models/solditem.go
+++ backend/models/solditem.go
@@ -6,7 +6,7 @@ import (
 
 type Solditem struct {
 	gorm.Model
-	ShopId int `gorm:"NOT NULL" json:"shop_id"`
-	ItemId int `gorm:"NOT NULL" json:"item_id"`
-	Amount int `gorm:"NOT NULL" json:"item_id"`
+	ShopId    int `gorm:"NOT NULL" json:"shop_id"`
+	ProductId int `gorm:"NOT NULL" json:"item_id"`
+	Amount    int `gorm:"NOT NULL" json:"item_id"`
 }
