fddcbe696addd80c59272e4eebd87904096c2df8
M.Sitthibudhi <sitthiput.m@gmail.com>
2021-03-13 20:00:20 +0700

    change password from bam's to boom's

--- backend/database/database.go
+++ backend/database/database.go
@@ -10,7 +10,7 @@ var DB *gorm.DB
 
 func Initdatabase() {
 	var err error
-	dsn := "host=localhost user=postgres password=ljoeljoe1123 dbname=empartydb port=5432 sslmode=disable TimeZone=Asia/Bangkok"
+	dsn := "host=localhost user=postgres password=1234 dbname=empartydb port=5432 sslmode=disable TimeZone=Asia/Bangkok"
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
