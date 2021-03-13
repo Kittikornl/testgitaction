3838b27bd67c991f9c188f973e8e8be650498a32
ppeamtas <teekataschaiburi@gmail.com>
2021-03-13 23:35:54 +0700

    edit entity name

--- backend/models/shoptable.go
+++ backend/models/shoptable.go
@@ -7,13 +7,13 @@ import (
 type Shoptable struct {
 	gorm.Model
 	// Auto-generated Shop id
-	UserData	int 	  `json:"userData"`
-	ShopName    string    `json:"shopname"`
-	Description string    `json:"description"`
-	PhoneNo     string    `json:"phone_number"`
-	Rating      int       `json:"rating"`
-	IG          string    `json:"ig"`
-	Facebook    string    `json:"facebook"`
-	Twitter     string    `json:"twitter"`
-	Line        string    `json:"line"`
+	UserID      int    `json:"userID"`
+	ShopName    string `json:"shopname"`
+	Description string `json:"description"`
+	PhoneNo     string `json:"phone_number"`
+	Rating      int    `json:"rating"`
+	IG          string `json:"ig"`
+	Facebook    string `json:"facebook"`
+	Twitter     string `json:"twitter"`
+	Line        string `json:"line"`
 }
