4c5aac31f93bd440a4ec2d8ac1ac42b5330cd252
M.Sitthibudhi <sitthiput.m@gmail.com>
2021-03-14 03:39:21 +0700

    Edit extract function

--- backend/services/common.go
+++ backend/services/common.go
@@ -4,19 +4,16 @@ import (
 	"github.com/dgrijalva/jwt-go"
 )
 
-func ExtractToken(tokenString string) (int, string) {
+// return userID(int), role(int)
+func ExtractToken(tokenString string) jwt.MapClaims {
 	claims := jwt.MapClaims{}
 	_, err := jwt.ParseWithClaims(tokenString, &claims, func(token *jwt.Token) (interface{}, error) {
 		// Verification key = 'Emparty'
-		return []byte("Emparty"), nil
+		return []byte("SECRET"), nil
 	})
 	// error handling
 	if err != nil {
 		println(err.Error())
 	}
-	// get attributes from decoded claims
-	email := claims["email"].(int)
-	role := claims["role"].(string)
-
-	return email, role
+	return claims
 }
