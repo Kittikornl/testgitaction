0676b182efb36b2c71fb50c4b99560e9b98b9a87
HathaichanokBam <h.nakarapanich@gmail.com>
2021-03-14 14:34:34 +0700

    add response field of products

--- backend/handle/user/home.go
+++ backend/handle/user/home.go
@@ -15,7 +15,7 @@ func GetHomePage(c *gin.Context) {
 	database.DB.Order("created_at desc").Limit(5).Find(&new_products)
 	database.DB.Model(models.Soldproduct{}).
 		Joins("left JOIN products on products.id =  soldproducts.product_id").
-		Select("products.id , products.product_title, sum(soldproducts.amount) as total").
+		Select("products.id, products.created_at, products.updated_at, products.deleted_at, products.shop_id, products.picture_url, products.product_title, products.price, products.amount, products.product_type, products.product_detail, products.rating, sum(soldproducts.amount) as total").
 		Group("products.id").Order("total desc").Limit(5).Scan(&top_selling_products)
 
 	c.JSON(http.StatusOK, gin.H{
