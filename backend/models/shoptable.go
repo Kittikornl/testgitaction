a3d5745a502e4e3358f02736b22184a5debef88a
M.Sitthibudhi <sitthiput.m@gmail.com>
2021-03-14 13:53:43 +0700

    Change type of rating from int to float64

--- backend/models/shoptable.go
+++ backend/models/shoptable.go
@@ -7,13 +7,13 @@ import (
 type Shoptable struct {
 	gorm.Model
 	// Auto-generated Shop id
-	UserID      int    `json:"user_id"`
-	ShopName    string `json:"shopname"`
-	Description string `json:"description"`
-	PhoneNo     string `json:"phone_number"`
-	Rating      int    `json:"rating"`
-	IG          string `json:"ig"`
-	Facebook    string `json:"facebook"`
-	Twitter     string `json:"twitter"`
-	Line        string `json:"line"`
+	UserID      int     `json:"user_id"`
+	ShopName    string  `json:"shopname"`
+	Description string  `json:"description"`
+	PhoneNo     string  `json:"phone_number"`
+	Rating      float64 `json:"rating"`
+	IG          string  `json:"ig"`
+	Facebook    string  `json:"facebook"`
+	Twitter     string  `json:"twitter"`
+	Line        string  `json:"line"`
 }
