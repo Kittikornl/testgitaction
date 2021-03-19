9e1ea9937d61d43d32bc9fbe47f6397962c3f4e1
pkorn <pkorn003@gmail.com>
2021-03-20 03:53:14 +0700

    Debug review API

--- backend/handle/reviews/review.go
+++ backend/handle/reviews/review.go
@@ -2,6 +2,7 @@ package reviews
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
@@ -25,10 +26,9 @@ func GetShopReviews(c *gin.Context) {
 	reviewsOutput := []ReviewOutput{}
 	shop_id := c.Param("id")
 
-	database.DB.Model(models.Userdata{}).
-		Joins("left JOIN shopreviews on userdata.id =  shopreviews.user_id").
-		Order("created_at desc").Where("shopreviews.shop_id = ? and len(shopreviews.comment) > 0", shop_id).
-		Limit(5).Scan(&reviewsOutput)
+	database.DB.Model(models.Shopreview{}).
+		Joins("left JOIN userdata on userdata.id =  shopreviews.user_id").
+		Order("created_at desc").Where("shopreviews.shop_id = ?", shop_id).Scan(&reviewsOutput)
 
 	c.JSON(http.StatusOK, reviewsOutput)
 }
@@ -37,32 +37,27 @@ func GetShopReviews(c *gin.Context) {
 func CreateShopReview(c *gin.Context) {
 	review := models.Shopreview{}
 	shop := models.Shoptable{}
-	totalReviews := 0 
 	//extract data from JWT
 	userID, _ := services.ExtractToken(c.GetHeader("Authorization"))
-	shop_id := c.Param("id")
-	
-	//find shop
-	if err := database.DB.First(&shop, shop_id).Error; err != nil {
-		c.JSON(http.StatusNotFound, services.ReturnMessage("shop_id: "+string(shop_id)+" does not exist"))
-		println(err.Error())
-		return
-	}
+	shopID := c.Param("id")
 
 	if err := c.ShouldBindBodyWith(&review, binding.JSON); err != nil {
 		c.Status(http.StatusBadRequest)
 		println(err.Error())
 		return
 	}
+	review.UserId = userID
+	shopIDInt, _ := strconv.Atoi(shopID)
+	review.ShopId = shopIDInt
 	
-	// count total Reviews
-	if err := database.DB.Model(models.Shopreview{}).Select("count(id)").Where("shop_id = ?", shop_id).Scan(&totalReviews); err != nil {
-		c.Status(http.StatusNotFound)
+	//find shop
+	if err := database.DB.First(&shop, shopID).Error; err != nil {
+		c.JSON(http.StatusNotFound, services.ReturnMessage("shop_id: "+string(shopID)+" does not exist"))
+		println(err.Error())
 		return
 	}
 
 	// Save data into reviews
-	review.UserId = userID
 	if err := database.DB.Save(&review).Error; err != nil {
 		c.Status(http.StatusInternalServerError)
 		println(err.Error())
@@ -70,8 +65,12 @@ func CreateShopReview(c *gin.Context) {
 	}
 	
 	// Update the data
-	shop.Rating = services.NewRating(shop.Rating, totalReviews, review.Rating)
-	if err := database.DB.Model(&models.Shoptable{}).Updates(&shop).Error; err != nil {
+	println(shop.Rating, shop.ReviewCount, review.Rating)
+	newRating := services.NewRating(shop.Rating, shop.ReviewCount, review.Rating)
+	shop.Rating = newRating
+	shop.ReviewCount = shop.ReviewCount + 1
+	println(shop.Rating, shop.ReviewCount, review.Rating)
+	if err := database.DB.Model(&models.Shoptable{}).Where("id = ?",shopID).Updates(&shop).Error; err != nil {
 		c.JSON(http.StatusBadRequest, services.ReturnMessage(err.Error()))
 		println(err.Error())
 		return
--- backend/services/review.go
+++ backend/services/review.go
@@ -5,6 +5,6 @@ import (
 )
 
 func NewRating(oldRating float64, totalReviews int, newRating int) float64{
-	rating := (oldRating*float64(totalReviews) +  float64(newRating)) / float64(totalReviews+1)
+	rating := ((oldRating*float64(totalReviews)) +  float64(newRating)) / float64(totalReviews+1)
 	return math.Round(rating*100)/100
 }
