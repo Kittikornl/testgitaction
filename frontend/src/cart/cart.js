ef63b89e6b439ca1b2fac870fc75dd8c54c75674
mintcnn <mintchnrt@gmail.com>
2021-03-21 16:59:56 +0700

    initialize review

---
+++ frontend/src/cart/cart.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react' 
+import './cart.scss'
+
+const Cart = () => {
+    return (
+        <div>
+            <Navbar/>
+        </div>
+    )
+}
+
+export default Cart
---
+++ frontend/src/review/review.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react' 
+import './review.scss'
+
+const Review = () => {
+    return (
+        <div>
+            <Navbar/>
+        </div>
+    )
+}
+
+export default Review
