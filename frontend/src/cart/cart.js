1a0046c3124a468767dea6b871422be09a5ce6d9
PICHCH K <pichchdev@gmail.com>
2021-03-21 19:45:40 +0700

    done ui cart

--- frontend/src/cart/cart.js
+++ frontend/src/cart/cart.js
@@ -1,13 +1,67 @@
-import React, { useState } from 'react' 
-import './cart.scss'
-import Navbar from '../components/navbar'
+import React, { useState } from "react";
+import "./cart.scss";
+import Navbar from "../components/navbar";
+import Searchbar from "../components/searchbar";
+import { Checkbox, Button } from "antd";
+import vegThumbnail from "../img/veg-thumbnail.jpg";
+import { DeleteOutlined } from "@ant-design/icons";
 
 const Cart = () => {
-    return (
-        <div>
-            <Navbar/>
+  return (
+    <div>
+      <Navbar />
+      <Searchbar />
+      <div className="cart-container">
+        <h2>My order</h2>
+        <div className="item-list">
+          <div className="item">
+            <div className="icon">
+              <Checkbox />
+            </div>
+            <div className="product-detail">
+              <div className="img">
+                <img src={vegThumbnail} />
+              </div>
+              <div className="desc">
+                <h3>Product Name mango mango mango</h3>
+                <h4>amount: 3 kg</h4>
+                <h4>price: 300 THB</h4>
+              </div>
+            </div>
+            <div className="icon text-right">
+              <DeleteOutlined className="delete-icon" />
+            </div>
+          </div>
+          <div className="item">
+            <div className="icon">
+              <Checkbox />
+            </div>
+            <div className="product-detail">
+              <div className="img">
+                <img src={vegThumbnail} />
+              </div>
+              <div className="desc">
+                <h3>Product Name mango mango mango</h3>
+                <h4>amount: 3 kg</h4>
+                <h4>price: 300 THB</h4>
+              </div>
+            </div>
+            <div className="icon text-right">
+              <DeleteOutlined className="delete-icon" />
+            </div>
+          </div>
         </div>
-    )
-}
+        <hr />
+        <div className="summary">
+          <h2>Total</h2>
+          <h1>500</h1>
+        </div>
+        <div className="text-center">
+          <Button htmlType="submit">Checkout</Button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Cart
+export default Cart;
