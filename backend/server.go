7ba6c88e77805f69071a0c70dd1d7166e519503d
M.Sitthibudhi <sitthiput.m@gmail.com>
2021-03-25 16:38:21 +0700

    debugging

--- backend/server.go
+++ backend/server.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"github.com/sec33_Emparty/backend/database"
-	"github.com/sec33_Emparty/backend/routers"
-	_ "github.com/sec33_Emparty/backend/docs" 
+	_ "github.com/sec33_Emparty/backend/docs"
 	_ "github.com/sec33_Emparty/backend/handle/products"
+	_ "github.com/sec33_Emparty/backend/handle/reviews"
 	_ "github.com/sec33_Emparty/backend/handle/shop"
 	_ "github.com/sec33_Emparty/backend/handle/user"
-	_ "github.com/sec33_Emparty/backend/handle/reviews"
+	"github.com/sec33_Emparty/backend/routers"
 )
 
 func main() {
