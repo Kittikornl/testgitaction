572f9ff5a23f9535a9d9baa5545c19a852de24bb
M.Sitthibudhi <sitthiput.m@gmail.com>
2021-03-25 23:11:12 +0700

    Add clearStock function

--- backend/handle/payment/payment.go
+++ backend/handle/payment/payment.go
@@ -5,7 +5,9 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
+	"github.com/sec33_Emparty/backend/database"
 	"github.com/sec33_Emparty/backend/models"
+	"github.com/sec33_Emparty/backend/services"
 )
 
 type Payment struct {
@@ -15,6 +17,11 @@ type Payment struct {
 
 func GetQR(c *gin.Context) {
 	payment := Payment{}
+
+	if !ClearStock(c) {
+		c.JSON(http.StatusBadRequest, services.ReturnMessage("Some order's amount is exceeding from the stock"))
+		return
+	}
 
 	// Change each order's status to 2 (paid, wait for being delivered)
 	for i, _ := range payment.Item {
@@ -38,6 +45,11 @@ func ValidateCard(c *gin.Context) {
 		return
 	}
 
+	if !ClearStock(c) {
+		c.JSON(http.StatusBadRequest, services.ReturnMessage("Some order's amount is exceeding from the stock"))
+		return
+	}
+
 	// Change each order's status to 2 (paid, wait for being delivered)
 	for i, _ := range payment.Item {
 		payment.Item[i].Status = 2
@@ -48,4 +60,48 @@ func ValidateCard(c *gin.Context) {
 		"orders":    payment.Item,
 		"promotion": payment.promotion,
 	})
+}
+
+func ClearStock(c *gin.Context) bool {
+	payment := Payment{}
+	product := models.Product{}
+
+	if err := c.ShouldBindBodyWith(&payment, binding.JSON); err != nil {
+		c.Status(http.StatusBadRequest)
+		println(err.Error())
+		return false
+	}
+
+	// for-loop to check if any order is over in amount from stock.
+	for _, order := range payment.Item {
+
+		if err := database.DB.Where("shop_id = ? AND product_title = ?", order.ShopID, order.ProductTitle).First(&product).Error; err != nil {
+			c.JSON(http.StatusNotFound, services.ReturnMessage(err.Error()))
+			return false
+		}
+
+		if int(order.Amount) > product.Amount {
+			c.JSON(http.StatusBadRequest, services.ReturnMessage("The requested amount is exceeding the stock's amount."))
+			return false
+		}
+	}
+
+	// for-loop to update the amount left for each stock
+	for _, order := range payment.Item {
+
+		if err := database.DB.Where("shop_id = ? AND product_title = ?", order.ShopID, order.ProductTitle).First(&product).Error; err != nil {
+			c.JSON(http.StatusNotFound, services.ReturnMessage(err.Error()))
+			return false
+		}
+
+		left_amount := product.Amount - int(order.Amount)
+		product.Amount = left_amount
+
+		if err := database.DB.Model(&product).Updates(product).Error; err != nil {
+			c.JSON(http.StatusBadRequest, services.ReturnMessage(err.Error()))
+			return false
+		}
+	}
+
+	return true
 }
