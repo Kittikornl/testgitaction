0d770b7db0ce3d2342919feacc8334942a4dba99
M.Sitthibudhi <sitthiput.m@gmail.com>
2021-03-29 17:48:37 +0700

    Add tracking number generating and save status to DB

--- backend/handle/payment/payment.go
+++ backend/handle/payment/payment.go
@@ -1,7 +1,9 @@
 package payment
 
 import (
+	"math/rand"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
@@ -36,6 +38,7 @@ type GetQRResponse struct {
 func GetQR(c *gin.Context) {
 	payment := Payment{}
 	response := GetQRResponse{}
+	lst := []models.Order{}
 
 	if err := c.ShouldBindBodyWith(&payment, binding.JSON); err != nil {
 		c.Status(http.StatusBadRequest)
@@ -49,15 +52,27 @@ func GetQR(c *gin.Context) {
 
 	// Change each order's status to 2 (paid, wait for being delivered)
 	for _, order := range payment.Item {
-		order.Status = 2
-		if err := database.DB.Model(&models.Order{}).Updates(&order).Error; err != nil {
+		orders := models.Order{}
+		if err := database.DB.Where("order_id = ?", order.OrderID).First(&orders).Error; err != nil {
+			c.JSON(http.StatusNotFound, services.ReturnMessage(err.Error()))
+			return
+		}
+		orders.Status = 2
+		orders.TrackingNumber = GenerateTrackingNumber()
+		lst = append(lst, orders)
+		println("$$$$$ TEST $$$$$")
+		println(order.Status)
+	}
+
+	for _, order := range lst {
+		if err := database.DB.Save(&order).Error; err != nil {
 			c.JSON(http.StatusBadRequest, services.ReturnMessage(err.Error()))
 			return
 		}
 	}
 
 	response.QR = "https://drive.google.com/file/d/1d9jKm7B71cleQNxgB6JawCKjQMv9jZzY/view?usp=sharing"
-	response.Order = payment.Item
+	response.Order = lst
 	response.Promotion = payment.promotion
 
 	// Fixed QR link, in the backend's GoogleDrive
@@ -73,6 +88,7 @@ func GetQR(c *gin.Context) {
 func ValidateCard(c *gin.Context) {
 	payment := Payment{}
 	response := ValidateCardResponse{}
+	lst := []models.Order{}
 
 	if err := c.ShouldBindBodyWith(&payment, binding.JSON); err != nil {
 		c.Status(http.StatusBadRequest)
@@ -86,15 +102,27 @@ func ValidateCard(c *gin.Context) {
 
 	// Change each order's status to 2 (paid, wait for being delivered)
 	for _, order := range payment.Item {
-		order.Status = 2
-		if err := database.DB.Model(&models.Order{}).Updates(&order).Error; err != nil {
+		orders := models.Order{}
+		if err := database.DB.Where("order_id = ?", order.OrderID).First(&orders).Error; err != nil {
+			c.JSON(http.StatusNotFound, services.ReturnMessage(err.Error()))
+			return
+		}
+		orders.Status = 2
+		orders.TrackingNumber = GenerateTrackingNumber()
+		lst = append(lst, orders)
+		println("$$$$$ TEST $$$$$")
+		println(order.Status)
+	}
+
+	for _, order := range lst {
+		if err := database.DB.Save(&order).Error; err != nil {
 			c.JSON(http.StatusBadRequest, services.ReturnMessage(err.Error()))
 			return
 		}
 	}
 
 	response.Acceptance = true
-	response.Order = payment.Item
+	response.Order = lst
 	response.Promotion = payment.promotion
 
 	c.JSON(http.StatusOK, response)
@@ -141,4 +169,22 @@ func ClearStock(c *gin.Context) bool {
 
 func LeftAmount(stock int, order int) int {
 	return stock - order
+}
+
+func GenerateTrackingNumber() string {
+	var tracking strings.Builder
+	num := "0123456789"
+	char := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+	for i := 0; i < 2; i++ {
+		tracking.WriteString(string(char[rand.Intn(26)]))
+	}
+
+	for i := 0; i < 9; i++ {
+		tracking.WriteString(string(num[rand.Intn(10)]))
+	}
+
+	tracking.WriteString("TH")
+
+	return tracking.String()
 }
