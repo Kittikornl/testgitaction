8483e4bf27002ed1b3230d9d53d48fd977fa733f
siras jaroenpoj <siras_2543@hotmail.com>
2021-04-05 13:27:04 +0700

    fix order id

--- frontend/src/history/history.js
+++ frontend/src/history/history.js
@@ -1,88 +1,94 @@
-import { Button } from 'antd'
-import React, { useEffect, useState } from 'react'
-import { useHistory } from 'react-router'
-import Searchbar from '../components/searchbar'
-import { getHistory } from '../service/cart.service'
+import { Button } from "antd";
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router";
+import Searchbar from "../components/searchbar";
+import { getHistory } from "../service/cart.service";
 
-import './history.scss'
+import "./history.scss";
 
-const HistoryItem = ({orderData, shopData}) => {
-    const history = useHistory()
-    const [shop] = useState(shopData.find(item => item.ID === orderData.shop_id));
+const HistoryItem = ({ orderData, shopData }) => {
+  const history = useHistory();
+  const [shop] = useState(
+    shopData.find((item) => item.ID === orderData.shop_id)
+  );
 
-    const handleClick = () => {
-        history.push("history/description", {
-            orderData : orderData,
-            shopData : shop
-        })
+  const handleClick = () => {
+    history.push("history/description", {
+      orderData: orderData,
+      shopData: shop,
+    });
+  };
+
+  const renderStatus = (status) => {
+    switch (status) {
+      case 0:
+        return "Wait for checkout";
+      case 1:
+        return "Wait for payment";
+      case 2:
+        return "paid";
+      case 3:
+        return "delivering";
+      case 4:
+        return "received";
+      case 5:
+        return "cancel";
     }
+  };
 
-    const renderStatus = (status) => {
-        switch(status) {
-            case 0 : return "Wait for checkout"
-            case 1 : return "Wait for payment"
-            case 2 : return "paid"
-            case 3 : return "delivering"
-            case 4 : return "received"
-            case 5 : return "cancel"
-        }
-    }
-
-    return (
-        <div className="history-item flex-row">
-            <div className="history-content-wrapper flex-row" >
-                <img src={orderData.picture_url} />
-                <div className="history-content flex-col">
-                    <div className="order-id">Order Id : {orderData.ID}</div>
-                    <div className="seller">Shop : {shop.shopname}</div>
-                    <div className="discount">Price : {orderData.price} TH</div>
-                    <div className="discount">Amount : {orderData.amount}</div>
-                    <div className="total">Total : {orderData.total_price} TH</div>
-                </div>
-            </div>
-            <div className="button-wrapper flex-col">
-                <div className="status">{renderStatus(orderData.status)}</div>
-                <Button onClick={() => handleClick()}>{"Click >"}</Button>
-            </div>
-            <div className="button-wrapper-mobile flex-col" >
-                <div className="status">{renderStatus(orderData.status)}</div>
-                <Button onClick={() => handleClick()}>{">"}</Button>
-            </div>
+  return (
+    <div className="history-item flex-row">
+      <div className="history-content-wrapper flex-row">
+        <img src={orderData.picture_url} />
+        <div className="history-content flex-col">
+          <div className="order-id">Order Id : {orderData.order_id}</div>
+          <div className="seller">Shop : {shop.shopname}</div>
+          <div className="discount">Price : {orderData.price} TH</div>
+          <div className="discount">Amount : {orderData.amount}</div>
+          <div className="total">Total : {orderData.total_price} TH</div>
         </div>
-    )
-}
+      </div>
+      <div className="button-wrapper flex-col">
+        <div className="status">{renderStatus(orderData.status)}</div>
+        <Button onClick={() => handleClick()}>{"Click >"}</Button>
+      </div>
+      <div className="button-wrapper-mobile flex-col">
+        <div className="status">{renderStatus(orderData.status)}</div>
+        <Button onClick={() => handleClick()}>{">"}</Button>
+      </div>
+    </div>
+  );
+};
 
 const History = () => {
+  const [data, setData] = useState([]);
 
-    const [data, setData] = useState([]);
+  useEffect(() => {
+    fetchdata();
+  }, []);
 
-    useEffect(() => {
-        fetchdata()
-    }, [])
+  const fetchdata = async () => {
+    const result = await getHistory();
+    console.log(result.data);
+    setData(result.data);
+  };
 
-    const fetchdata = async () => {
-        const result = await getHistory()
-        console.log(result.data)
-        setData(result.data)
-    }
+  const renderHistoryItem = (item, idx) => {
+    return <HistoryItem key={idx} orderData={item} shopData={data.shop_info} />;
+  };
 
-    const renderHistoryItem = (item, idx) => {
-        return <HistoryItem key={idx} orderData={item} shopData={data.shop_info}/>
-    }
-
-    if(!data.order_info)
-    return <div></div>
-    return (
-        <div className="history-page-container flex-col">
-            <Searchbar />
-            <div className="history-container">
-                <h1>My history</h1>
-                <div className="history-item-container flex-col">
-                    {data.order_info.map(renderHistoryItem)}
-                </div>
-            </div>
+  if (!data.order_info) return <div></div>;
+  return (
+    <div className="history-page-container flex-col">
+      <Searchbar />
+      <div className="history-container">
+        <h1>My history</h1>
+        <div className="history-item-container flex-col">
+          {data.order_info.map(renderHistoryItem)}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default History
+export default History;
--- frontend/src/history/historyDesc.js
+++ frontend/src/history/historyDesc.js
@@ -1,108 +1,111 @@
-import { Button } from 'antd'
-import React, { useEffect, useState } from 'react'
-import { useHistory } from 'react-router'
+import { Button } from "antd";
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router";
 
-import './historyDesc.scss'
+import "./historyDesc.scss";
 
 const HistoryDesc = (props) => {
+  const history = useHistory();
+  const [orderData, setOrderData] = useState();
+  const [shopdata, setShopData] = useState();
 
-    const history = useHistory()
-    const [orderData, setOrderData] = useState();
-    const [shopdata, setShopData] = useState();
+  useEffect(() => {
+    const receiveProps = props.location.state;
+    console.log(receiveProps);
+    setOrderData(receiveProps.orderData);
+    setShopData(receiveProps.shopData);
+  }, []);
 
-    useEffect(() => {
-        const receiveProps = props.location.state
-        console.log(receiveProps)
-        setOrderData(receiveProps.orderData)
-        setShopData(receiveProps.shopData)
-    }, []);
+  const handleMakePayment = () => {
+    history.push("/checkout", {
+      orderData: orderData,
+      shopData: shopdata,
+    });
+  };
 
-    const handleMakePayment = () => {
-        history.push("/checkout", {
-            orderData : orderData,
-            shopData : shopdata
-        })
-    }
-
-    if(!orderData)
-    return <div></div>
-    return (
-        <div className="history-desc-container flex-col">
-            <div className="order-title flex-row">
-                <h1>Order Id : {orderData.ID}</h1>
-                {1===0? 
-                    <div className="success-paid flex-row">
-                        <Button>Track your order</Button>
-                        <Button>Review</Button>
-                    </div> :
-                    <div className="unsuccess-paid">
-                        <Button onClick={()=>handleMakePayment()}>Make a payment</Button>
-                    </div>
-                }
-            </div>
-            <div className="order-content flex-col">
-                <div className="content-warpper flex-row">
-                    <div className="title flex-col">
-                        <div>Shop : </div>
-                        <div>Tel : </div>
-                    </div>
-                    <div className="content  flex-col">
-                        <div>{shopdata.shopname}</div>
-                        <div>0614807734</div>
-                    </div>
-                </div>
-                <div className="content-warpper flex-row">
-                    <div className="title flex-col">
-                        <div>Name : </div>
-                        <div>Address : </div>
-                        <div>Tel : </div>
-                    </div>
-                    <div className="content flex-col">
-                        <div>Tinnapop Pratheep</div>
-                        <div>Chulalongkorn University</div>
-                        <div>0614807734</div>
-                    </div>
-                </div>
-                <div className="content-warpper flex-row">
-                    <div className="title flex-col">
-                        <div>Tracking number : </div>
-                        <div>Shipping : </div>
-                    </div>
-                    <div className="content flex-col">
-                        <div>{orderData.tracking_number === "" ? "-" : orderData.tracking_number}</div>
-                        <div>Kerry</div>
-                    </div>
-                </div>
-                <div className="content-warpper flex-col">
-                    <div className="title-product">Product</div>
-                    <div className="product-item flex-row">
-                        <img src={orderData.picture_url} />
-                        <div className="product-content flex-col">
-                            <div>{orderData.product_title}</div>
-                            <div>Amount : {orderData.amount} kg</div>
-                            <div>Price : {orderData.total_price} TH</div>
-                        </div>
-                    </div>
-                </div>
-                <div className="content-warpper flex-row">
-                    <div className="title flex-col">
-                        <div>Subtotal : </div>
-                        <div>Discount : </div>
-                        <div>Total : </div>
-                        {1===1 && <div>Payment method : </div>}
-                        {1===1 && <div>Payment status : </div>}
-                    </div>
-                    <div className="content flex-col">
-                        <div>{orderData.total_price} TH</div>
-                        <div>40 TH</div>
-                        <div>50 TH</div>
-                        {1===1 && <div>Credit card</div>}
-                        {1===1 && <div>Already</div>}
-                    </div>
-                </div>
-            </div>
+  if (!orderData) return <div></div>;
+  return (
+    <div className="history-desc-container flex-col">
+      <div className="order-title flex-row">
+        <h1>Order Id : {orderData.order_id}</h1>
+        {1 === 0 ? (
+          <div className="success-paid flex-row">
+            <Button>Track your order</Button>
+            <Button>Review</Button>
+          </div>
+        ) : (
+          <div className="unsuccess-paid">
+            <Button onClick={() => handleMakePayment()}>Make a payment</Button>
+          </div>
+        )}
+      </div>
+      <div className="order-content flex-col">
+        <div className="content-warpper flex-row">
+          <div className="title flex-col">
+            <div>Shop : </div>
+            <div>Tel : </div>
+          </div>
+          <div className="content  flex-col">
+            <div>{shopdata.shopname}</div>
+            <div>0614807734</div>
+          </div>
         </div>
-    )
-}
+        <div className="content-warpper flex-row">
+          <div className="title flex-col">
+            <div>Name : </div>
+            <div>Address : </div>
+            <div>Tel : </div>
+          </div>
+          <div className="content flex-col">
+            <div>Tinnapop Pratheep</div>
+            <div>Chulalongkorn University</div>
+            <div>0614807734</div>
+          </div>
+        </div>
+        <div className="content-warpper flex-row">
+          <div className="title flex-col">
+            <div>Tracking number : </div>
+            <div>Shipping : </div>
+          </div>
+          <div className="content flex-col">
+            <div>
+              {orderData.tracking_number === ""
+                ? "-"
+                : orderData.tracking_number}
+            </div>
+            <div>Kerry</div>
+          </div>
+        </div>
+        <div className="content-warpper flex-col">
+          <div className="title-product">Product</div>
+          <div className="product-item flex-row">
+            <img src={orderData.picture_url} />
+            <div className="product-content flex-col">
+              <div>{orderData.product_title}</div>
+              <div>Amount : {orderData.amount} kg</div>
+              <div>Price : {orderData.total_price} TH</div>
+            </div>
+          </div>
+        </div>
+        <div className="content-warpper flex-row">
+          <div className="title flex-col">
+            <div>Subtotal : </div>
+            <div>Discount : </div>
+            <div>Total : </div>
+            {1 === 1 && <div>Payment method : </div>}
+            {1 === 1 && <div>Payment status : </div>}
+          </div>
+          <div className="content flex-col">
+            <div>{orderData.total_price} TH</div>
+            <div>40 TH</div>
+            <div>50 TH</div>
+            {1 === 1 && <div>Credit card</div>}
+            {1 === 1 && <div>Already</div>}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default HistoryDesc
+export default HistoryDesc;
